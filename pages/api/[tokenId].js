485426b97806044a4a1b488d5079eb8b5ccd5095
Yusuf Elbana <yuselbana1@gmail.com>
2022-07-08 16:13:19 -0400

    Initial Commit

---
+++ pages/api/[tokenId].js
@@ -0,0 +1,9 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  const tokenId = req.query.tokenId;
+const name = `Miku #{tokenId}`;
+const description = "Miku is a pog NFT Collection."
+const image = "./"
+  
+}
